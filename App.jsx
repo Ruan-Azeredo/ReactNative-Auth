0ebaa20c6aff04971d735433318a48ff97995cac
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-22 10:29:13 -0300

    :construction: build: js -> jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { NativeBaseProvider } from 'native-base';
+import { Router } from './src/routes/Router';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Router/>
+    </NativeBaseProvider>
+  );
+}
