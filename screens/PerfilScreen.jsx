49acfe168f939b60b1652f3d0b053909f5fc06b4
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-24 11:25:08 -0300

    :construction: build: PerfilScreen

---
+++ screens/PerfilScreen.jsx
@@ -0,0 +1,10 @@
+import { View } from "native-base";
+import { Text } from "react-native";
+
+export function PerfilScreen() {
+    return (
+        <View className="h-full">
+            <Text className="m-auto">Olá Ruan</Text>
+        </View>
+    )
+}
