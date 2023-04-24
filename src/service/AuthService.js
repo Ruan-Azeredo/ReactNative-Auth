bb905740136406c577de7bfb9a7e4e7908cb2b76
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-24 17:52:26 -0300

    :sparkles: feat: cria email de admin

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -2,11 +2,19 @@ async function signIn(email, password) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (password == '1234') {
-                resolve({
-                    token: 'exemple-token',
-                    email: 'exemple-email',
-                    name: 'Ruan Azeredo'
-                })
+                if (email == 'admin@gmail.com') {
+                    resolve({
+                        token: 'exemple-token',
+                        email: 'admin@gmail.com',
+                        name: 'Ruan Azeredo'
+                    })
+                } else {
+                    resolve({
+                        token: 'exemple-token',
+                        email: 'exemple-email',
+                        name: 'Ruan Azeredo'
+                    })
+                }
             } else {
                 reject(new Error('Email ou Senha incorreta'))
             }
