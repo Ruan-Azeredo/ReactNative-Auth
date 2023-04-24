c951e9ff72870a143dc2bbdb500aba9420753009
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-04-24 17:52:06 -0300

    :sparkles: feat: atualiza logica niveis de acesso

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,12 +3,20 @@ import { UserStack } from './UserStack';
 import { AuthStack } from './AuthStack';
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/Auth';
+import { AdminStack } from './AdminStack';
 
 export function Router() {
     const { authData } = useContext(AuthContext)
     return (
         <NavigationContainer>
-            {authData != undefined ? <UserStack/> : <AuthStack/>}
+            {authData == undefined ?
+                <AuthStack />
+                :
+                authData.email == 'admin@gmail.com' ?
+                    <AdminStack />
+                    :
+                    <UserStack />
+            }
         </NavigationContainer>
     )
 }
